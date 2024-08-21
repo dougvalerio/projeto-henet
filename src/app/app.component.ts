3f93897186bb8e8cb4751c366fd1d0ec493ac31c
Douglas <douglas.br80@hotmail.com>
2024-08-21 16:00:46 -0300

    Removido o footer, adicionado a logo da velejar na página inicial, deixado o header transparente, iniciado o carregamento das imagens na galeria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import { HeaderComponent } from "./components/header/header.component";
 import { SliderComponent } from './components/slider/slider.component';
 import { GaleriaComponent } from './components/galeria/galeria.component';
 import { UploadComponent } from './components/upload/upload.component';
-import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SliderComponent, GaleriaComponent, UploadComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, SliderComponent, GaleriaComponent, UploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -15,7 +15,9 @@ import { Imagem } from '../../models/imagem';
 export class GaleriaComponent implements OnInit {
   isPopupOpen = false;
   selectedImageSrc: SafeUrl | null = null;
+  selectedQrcodeSrc: SafeUrl | null = null;  // Adicionando variável para o QR code
   imagens: SafeUrl[] = [];  // Array para armazenar URLs seguras das imagens
+  fotoAtualId: number | null = null;  // Armazena o ID da foto atual
 
   constructor(private fotosService: FotosService, private sanitizer: DomSanitizer) {}
 
@@ -25,7 +27,16 @@ export class GaleriaComponent implements OnInit {
 
   carregarImagens(): void {
     this.fotosService.getAllFotos().subscribe((fotos: Imagem[]) => {
+      console.log("Exibir fotos", fotos)
+      // Ordenar as fotos por ID em ordem decrescente
+      fotos.sort((a, b) => b.id - a.id);
+  
+      // Limpa o array para evitar duplicação se o método for chamado novamente
+      this.imagens = [];
+  
+      // Itera sobre as fotos já ordenadas
       fotos.forEach(foto => {
+        console.log("Teste: ", foto)
         this.fotosService.getFoto(foto.id).subscribe((imagemBlob: Blob) => {
           const objectURL = URL.createObjectURL(imagemBlob);
           const sanitizedUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
@@ -35,13 +46,28 @@ export class GaleriaComponent implements OnInit {
     });
   }
 
-  openImagePopup(imageSrc: SafeUrl): void {
+  openImagePopup(imageSrc: SafeUrl, fotoId: number): void {
     this.selectedImageSrc = imageSrc;
+    this.fotoAtualId = fotoId;  // Armazena o ID da foto atual
     this.isPopupOpen = true;
+
+    // Exibe no console o ID da foto ao abrir o popup
+    console.log('ID da Foto:', fotoId);
+
+    // Carregar o QR code baseado no ID da foto
+    this.fotosService.getQrcodeById(fotoId).subscribe((qrcodeBlob: Blob) => {
+      const objectURL = URL.createObjectURL(qrcodeBlob);
+      this.selectedQrcodeSrc = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+      
+      // Exibe no console o ID do QR code após carregá-lo
+      console.log('ID do QR Code:', fotoId);
+    });
   }
 
   closeImagePopup(): void {
     this.isPopupOpen = false;
     this.selectedImageSrc = null;
+    this.selectedQrcodeSrc = null;  // Limpar a URL do QR code
+    this.fotoAtualId = null;  // Limpar o ID da foto atual
   }
 }
