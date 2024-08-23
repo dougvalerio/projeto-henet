174f8f733c3dd3829af3cc200c0974b981865334
Douglas <douglas.br80@hotmail.com>
2024-08-23 16:01:49 -0300

    Adicionado mais uma tela de qrcode no header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { SliderComponent } from './components/slider/slider.component';
 import { GaleriaComponent } from './components/galeria/galeria.component';
+import { QrcodeComponent } from './components/qrcode/qrcode.component';
 import { UploadComponent } from './components/upload/upload.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SliderComponent, GaleriaComponent, UploadComponent],
+  imports: [RouterOutlet, HeaderComponent, SliderComponent, GaleriaComponent, QrcodeComponent, UploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { SliderComponent } from './components/slider/slider.component';
 import { GaleriaComponent } from './components/galeria/galeria.component';
 import { UploadComponent } from './components/upload/upload.component';
+import { QrcodeComponent } from './components/qrcode/qrcode.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redireciona o caminho vazio para /home
   { path: 'home', component: SliderComponent },
   { path: 'galeria', component: GaleriaComponent },
+  { path: 'qrcode', component: QrcodeComponent },
   { path: 'upload', component: UploadComponent },
   // Adicione um wildcard route para redirecionar para /home em caso de rota inválida
   { path: '**', redirectTo: '/home' }
