a9b371a513a37910d33c47e48e32683e2b321c01
Douglas <douglas.br80@hotmail.com>
2024-09-17 11:38:27 -0300

    Criado a tela de feedback

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './feedback.component.css'
 })
 export class FeedbackComponent {
+  selectedFeedback: number = 0;
+  opinion: string = '';
 
+  setFeedback(rating: number) {
+    this.selectedFeedback = rating;
+  }
+
+  submitFeedback() {
+    if (this.selectedFeedback === 0 || this.opinion.trim() === '') {
+      alert('Por favor, selecione uma avaliação e escreva sua opinião.');
+      return;
+    }
+
+    // Lógica para salvar o feedback
+    console.log('Feedback:', this.selectedFeedback);
+    console.log('Opinião:', this.opinion);
+
+    alert('Feedback enviado com sucesso!');
+    // Limpar campos após o envio
+    this.selectedFeedback = 0;
+    this.opinion = '';
+  }
 }
