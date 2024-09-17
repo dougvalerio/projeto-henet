0f936487156dfc94e4ccf58eb2503299e58a78bf
Douglas <douglas.br80@hotmail.com>
2024-09-17 10:22:56 -0300

    Adicionado a tela de Carrossel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { GaleriaComponent } from './components/galeria/galeria.component';
 import { UploadComponent } from './components/upload/upload.component';
 import { QrcodeComponent } from './components/qrcode/qrcode.component';
 import { ConfigComponent } from './components/config/config.component';
+import { CarrosselComponent } from './components/carrossel/carrossel.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redireciona o caminho vazio para /home
   { path: 'home', component: SliderComponent },
+  { path: 'carrossel', component: CarrosselComponent },
   { path: 'galeria', component: GaleriaComponent },
   { path: 'qrcode', component: QrcodeComponent },
   { path: 'upload', component: UploadComponent },
