13724d246c247eb0b8c8ce0aa9d08fffc37629e3
Fábio P Almeida <fabiopereiraalmeida@gmail.com>
2024-09-18 14:16:54 -0300

    up

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -6,8 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ConfigService {
-
-  private baseUrl = 'http://177.38.244.53:9090/api/config'; // Substitua pela URL real do backend
+  
+  //private apiUrl = 'http://177.38.244.53:9090/api/config'; // Substitua pela URL real do backend
+  private apiUrl = 'http://localhost:8080/api/config';
 
   constructor(private http: HttpClient) { }
 
@@ -16,7 +17,7 @@ export class ConfigService {
     const formData: FormData = new FormData();
     formData.append('file', file);
 
-    return this.http.post<string>(`${this.baseUrl}/uploadBackground`, formData);
+    return this.http.post<string>(`${this.apiUrl}/uploadBackground`, formData);
   }
 
   // Método para fazer o upload da moldura
@@ -24,16 +25,16 @@ export class ConfigService {
     const formData: FormData = new FormData();
     formData.append('file', file);
 
-    return this.http.post<string>(`${this.baseUrl}/uploadMoldura`, formData);
+    return this.http.post<string>(`${this.apiUrl}/uploadMoldura`, formData);
   }
 
   // Método para buscar o background
   getBackground(): Observable<Blob> {
-    return this.http.get(`${this.baseUrl}/background`, { responseType: 'blob' });
+    return this.http.get(`${this.apiUrl}/background`, { responseType: 'blob' });
   }
 
   // Método para buscar a moldura
   getMoldura(): Observable<Blob> {
-    return this.http.get(`${this.baseUrl}/moldura`, { responseType: 'blob' });
+    return this.http.get(`${this.apiUrl}/moldura`, { responseType: 'blob' });
   }
 }
